c659d1b9c9f1e12feb255eec1287799f71538ece
Maryem Bamou <34491538+int-j@users.noreply.github.com>
2021-10-31 12:53:18 +0100

    Create intro.py

---
+++ Introduction to Programming with Python/intro.py
@@ -0,0 +1,4 @@
+print("hello world!")
+print("we will share here a resume about this course, with some side projects")
+#students od IU - B.Sc Cybersecurity feel free to add/change some code! 
+      
